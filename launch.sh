8921fdc0a9871fc467546f2159f0a8703d3556ef
Shing Lyu <shing.lyu@gmail.com>
2015-10-21 16:02:31 +0800

    Extract the provision script to a separate file

--- launch.sh
+++ launch.sh
@@ -2,10 +2,6 @@ THIS_REPO_URL=$(git config --get remote.origin.url | sed 's/git@github.com:/http
 cd vm
 vagrant up
 VM_SHELL="vagrant ssh -c"
-$VM_SHELL "sudo apt-get install -y git"
-$VM_SHELL "sudo ssh-keyscan github.com >> ~/.ssh/known_hosts"
-$VM_SHELL "rm -rf MozITP" # Cleanup old repo
-$VM_SHELL "git clone $THIS_REPO_URL" #TODO: Do we need special folder?
 
+$VM_SHELL "cat | bash /dev/stdin $THIS_REPO_URL" < ../scripts/provision.sh  
 $VM_SHELL "cd ./MozITP/scripts/; ./menu.sh"
-# cd ..
---
+++ scripts/provision.sh
@@ -0,0 +1,7 @@
+#ITP_REPO_URL="https://github.com/Mozilla-TWQA/MozITP"
+ITP_REPO_URL=$1
+sudo apt-get install -y git
+sudo ssh-keyscan github.com >> ~/.ssh/known_hosts
+rm -rf MozITP # Cleanup old rep
+git clone $ITP_REPO_URL #TODO: Do we need special folder
+
