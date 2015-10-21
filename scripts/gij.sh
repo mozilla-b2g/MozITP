1c475ae351004f250fff1e3927be5aeec6c5fd5b
Shing Lyu <shing.lyu@gmail.com>
2015-10-21 19:30:37 +0800

    Added start x window script; updated gij

--- scripts/gij.sh
+++ scripts/gij.sh
@@ -1,1 +1,12 @@
-echo "NOT IMPLEMENTED YET"
+# Install NVM
+sudo apt-get install install build-essential libssl-dev
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.26.1/install.sh | bash #Notice the version may change
+source ~/.nvm/nvm.sh
+nvm install 0.12
+nvm use 0.12
+
+# Get gaia code
+sudo apt-get install -y libfontconfig1 libasound2 libgtk2.0-0 python-pip
+git clone https://github.com/mozilla-b2g/gaia.git
+cd gaia
+make test-integration
---
+++ scripts/startgui.sh
@@ -0,0 +1,3 @@
+sudo apt-get install -y lxde-core lightdm-gtk-greeter xinit
+update-rc.d -f lightdm remove
+sudo startx
