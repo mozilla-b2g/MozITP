87328e5a18fe5fe2758f8509a2715f366d44b844
askeing <askeing@gmail.com>
2015-11-04 18:02:32 +0800

    Add Android Rules for B2G devices

--- scripts/install_adb_fastboot.sh
+++ scripts/install_adb_fastboot.sh
@@ -2,3 +2,17 @@
 
 # install adb and fastboot
 sudo apt-get -y install android-tools-adb android-tools-fastboot
+
+# set up the android rules
+sudo rm -rf /etc/udev/rules.d/57-android.rules
+sudo touch /etc/udev/rules.d/57-android.rules
+
+sudo echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="19d2", MODE="0666", GROUP="plugdev"' >> /etc/udev/rules.d/57-android.rules
+sudo echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", MODE="0666", GROUP="plugdev"' >> /etc/udev/rules.d/57-android.rules
+sudo echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="05c6", MODE="0666", GROUP="plugdev"' >> /etc/udev/rules.d/57-android.rules
+sudo echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="1004", MODE="0666", GROUP="plugdev"' >> /etc/udev/rules.d/57-android.rules
+sudo echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="1782", MODE="0666", GROUP="plugdev"' >> /etc/udev/rules.d/57-android.rules
+sudo echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="2717", MODE="0666", GROUP="plugdev"' >> /etc/udev/rules.d/57-android.rules
+sudo echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="0fce", MODE="0666", GROUP="plugdev"' >> /etc/udev/rules.d/57-android.rules
+
+sudo service udev restart
