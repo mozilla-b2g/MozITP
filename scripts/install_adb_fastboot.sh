34918ced136b858cf92a26d27a132e2c5fc5f818
askeing <askeing@gmail.com>
2015-11-05 12:14:25 +0800

    Move android rules file to config_files folder

--- scripts/install_adb_fastboot.sh
+++ scripts/install_adb_fastboot.sh
@@ -5,14 +5,15 @@ sudo apt-get -y install android-tools-adb android-tools-fastboot
 
 # set up the android rules
 sudo rm -rf /etc/udev/rules.d/57-android.rules
-sudo touch /etc/udev/rules.d/57-android.rules
 
-sudo echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="19d2", MODE="0666", GROUP="plugdev"' >> /etc/udev/rules.d/57-android.rules
-sudo echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", MODE="0666", GROUP="plugdev"' >> /etc/udev/rules.d/57-android.rules
-sudo echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="05c6", MODE="0666", GROUP="plugdev"' >> /etc/udev/rules.d/57-android.rules
-sudo echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="1004", MODE="0666", GROUP="plugdev"' >> /etc/udev/rules.d/57-android.rules
-sudo echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="1782", MODE="0666", GROUP="plugdev"' >> /etc/udev/rules.d/57-android.rules
-sudo echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="2717", MODE="0666", GROUP="plugdev"' >> /etc/udev/rules.d/57-android.rules
-sudo echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="0fce", MODE="0666", GROUP="plugdev"' >> /etc/udev/rules.d/57-android.rules
+# get current file's folder
+CUR_DIR=`dirname $0`
+# get parent of current file's folder
+CUR_PARENT_DIR=`dirname ${CUR_DIR}`
+# copy config file of android rules to etc/udev/rules.d folder
+sudo cp ${CUR_PARENT_DIR}/config_files/57-android.rules /etc/udev/rules.d/57-android.rules
+# change owner and mode
+sudo chown root:root /etc/udev/rules.d/57-android.rules
+sudo chmod a+x /etc/udev/rules.d/57-android.rules
 
 sudo service udev restart
