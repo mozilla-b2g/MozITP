057dc1c508f3ba079e9e62b86c42dae04dcbf9bd
Shing Lyu <shing.lyu@gmail.com>
2015-11-18 14:58:34 +0800

    Added adb info

--- scripts/check_gij_version.sh
+++ scripts/check_gij_version.sh
@@ -11,7 +11,12 @@ echo -en "python version:\t"
 python --version
 echo -en "pip version:\t"
 pip --version
+echo -en "adb version:\t"
+adb version
 echo -en "Linux version:\t"
 uname -s -r -v -m -p -i -o
+
+echo "\"adb devices\" output:"
+adb devices
 
 
