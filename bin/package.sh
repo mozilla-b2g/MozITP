babf59655f8cf9862b87ece3c6d43e39817885b6
Shing Lyu <shing.lyu@gmail.com>
2016-01-13 15:53:29 +0800

    Trying to enable packaging

---
+++ bin/package.sh
@@ -0,0 +1,12 @@
+OUTPUT=/tmp/mozitp_$(git rev-parse --short HEAD).box
+
+# Need to be run in the root dir
+./reset_vm.sh
+./bin/gij_sanity_test.sh
+# ./bin/gip_sanity_test.sh
+# TODO: b2g-installer + X-window
+cd vm
+#DEBUG
+vagrant ssh -c "sudo bash ~/MozITP/scripts/package/cleanup.sh"
+vagrant package --output $OUTPUT
+ls -lh $OUTPUT
