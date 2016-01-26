05cb26c1c1f8bed9f63b7beff7cb1d4345c101eb
askeing <askeing@gmail.com>
2016-01-26 14:42:44 +0800

    Update the get credentials scripts and screenshot

--- bin/get_credentials.sh
+++ bin/get_credentials.sh
@@ -10,24 +10,21 @@ then
 fi
 
 # create and activate env
-rm -rf env-python
 virtualenv env-python
 source ./env-python/bin/activate
 
 # install packages
-pip install -U b2g-util taskcluster-util
+pip install -U pip b2g-util taskcluster-util
 
 # run CLI and login
-echo "Please install 'tkinter' if you do not install it."
-taskcluster_login --file ./shared/tc_credentials.json
+taskcluster_login
 
 # deactivate
 deactivate
 
 # copy credentials file into vm
-# cd vm
 vagrant up
-vagrant scp ./shared/tc_credentials.json default:/home/vagrant/tc_credentials.json
+vagrant scp ~/tc_credentials.json default:/home/vagrant/tc_credentials.json
 
 # halt VM or not.
 read -p "Let VM keep running? [Y/n]" CHOICE
