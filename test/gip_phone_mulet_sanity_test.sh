fc1c0d13d79c44f042be52565677e8172ecaec20
askeing <askeing@gmail.com>
2016-01-29 13:51:48 +0800

    Change the sanity test file of GIP phone mulet
    - Base on manifest.ini, test_clock_run_stopwatch_laps.py should be skiped when device is desktop b2g.
    ref: https://github.com/mozilla-b2g/gaia/blob/master/tests/python/gaia-ui-tests/gaiatest/tests/functional/clock/manifest.ini

--- test/gip_phone_mulet_sanity_test.sh
+++ test/gip_phone_mulet_sanity_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-export TEST_FILES=gaiatest/tests/functional/clock/test_clock_run_stopwatch_laps.py
+export TEST_FILES=gaiatest/tests/functional/clock/test_clock_run_stopwatch_then_reset.py
 source ./launch.sh gip #use source to get the correct error code
